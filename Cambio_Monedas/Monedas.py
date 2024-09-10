0b9a36aa072834bbb35c11460aaa75facdb6c1e2
julian7965_Udea <julian.tobon2@udea.edu.co>
2024-09-09 23:31:03 -0500

    Cambio ruta repositorio

--- Cambio_Monedas/Monedas.py
+++ Cambio_Monedas/Monedas.py
@@ -1,9 +1,22 @@
 #Importar libreria para interfaz grafica de usuario
 from tkinter import *
+#Importar Util para las funciones ya creadas
+import Util
+
+iconos=["./iconos/bd.png", \
+        "./iconos/statistics.png"] #El espacio + backslash es para partir codigo en varias lineas
 
 #Crear una ventana
 v = Tk()
 
 #Titulo ventana
 v.title("Cambios de Monedas")
+
+#Tamaño ventana
 v.geometry("400x300")
+
+#Importar función
+Util.agregarBarra(v,iconos)
+
+# Iniciar el bucle de la aplicación para que la ventana se mantenga visible
+v.mainloop()
